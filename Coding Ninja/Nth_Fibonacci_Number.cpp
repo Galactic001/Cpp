a4139785c5c02a4423d2524a480556fc44d9eb6e
Ayush <ayushpradhan123n@gmail.com>
2024-08-19 16:01:05 +0530

    Update Nth_Fibonacci_Number.cpp

--- Coding Ninja/Nth_Fibonacci_Number.cpp
+++ Coding Ninja/Nth_Fibonacci_Number.cpp
@@ -1,30 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-        /*
-        *  Write your code here. 
-         *  Read input as specified in the question.
-         *  Print output as specified in the question.
-        */
-        int n, i, t1 = 0, t2 = 1, nextTerm = 0;
+    /*
+     *  Write your code here.
+     *  Read input as specified in the question.
+     *  Print output as specified in the question.
+     */
+    int n, i, t1 = 0, t2 = 1, nextTerm = 0;
 
-        cin >> n;
+    cin >> n;
 
-        for (i = 1; i <= n; i++) {
-                if (i == 1) {
-                        continue;
-                }
-                if (i == 2) {
-                        continue;
-                }
-                nextTerm = t1 + t2;
-                t1 = t2;
-                t2 = nextTerm;
-                }
+    for (i = 1; i <= n; i++)
+    {
+        if (i == 1)
+        {
+            nextTerm = t2;
+            continue;
+        }
+        else
+        {
+            nextTerm = t1 + t2;
+            t1 = t2;
+            t2 = nextTerm;
+        }
+    }
 
-        std::cout << nextTerm << std::endl;
+    std::cout << nextTerm << std::endl;
 
-        return 0;
+    return 0;
 }
